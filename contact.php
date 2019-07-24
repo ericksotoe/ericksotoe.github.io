c6dc45e590b88b4aa64e60817e0e5ce929c69399
erick <ericksotoe@gmail.com>
2019-07-24 00:46:55 -0700

    checking more advance php request

--- contact.php
+++ contact.php
@@ -1,5 +1,17 @@
 <?php
-if($_POST["message"]) {
-    mail("ericksotoe@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
+
+if($_POST["submit"]) {
+    $recipient="ericksotoe@gmail.com";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
+
 ?>
